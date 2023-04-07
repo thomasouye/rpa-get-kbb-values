827246f1654464080c974370fbd191abb89ea0ff
Thomas Ouye <thomas.ouye@servco.com>
2023-04-07 12:35:07 -1000

    Code cleanup and allow JSON batching

--- app.py
+++ app.py
@@ -39,83 +39,56 @@ if not "kbb_api_key" in os.environ:
 @app.route("/", methods=["POST"])
 def run() -> str:
     kbb = Kbb(os.environ["kbb_api_key"])
+    dataReader = VehicleDataReader()
+    #limit used to cap the max number of calls
+    #limit = float("inf")
+    limit = 3
+
     if request.is_json:
         data = request.get_json()
-        vin = data["vin"]
-        mileage = data["mileage"]
-        zipCode = data["zip"]
-        make = data["make"]
-        model = data["model"]
-        year = data["year"]
-        trim = data["trim"]
-        vehicleOptions = []
-        if "options" in data:
-            vehicleOptions = data["options"]
-
-        # try:
-        #     return kbb.getValueByVin(vin, mileage, zipCode)
-        # except Exception as e:
-        #     return str(e)
-        errors = []
-        values = []
-        modelMatchesVin = False
-
-
-        if vin:
-            # values = kbb.getValueByVinAndTrim(vin, trim, mileage, zipCode)
-            # vehicleOptions = kbb.getVehicleByVinAndTrim(vin, trim)
-            return kbb.getVehicleValueReportByVINAndTrim(vin, trim, mileage, zipCode, vehicleOptions)
-
-            # try:
-            #     modelMatchesVin = kbb.compareVehicleVinAndName(vin, year, make, model, trim)
-            # except Exception as e:
-            #     errors.append(str(e))
-        else:
-            try:
-                values = kbb.getValueByName(year, make, model, trim, mileage, zipCode)
-            except Exception as e:
-                errors.append(str(e))
-
-        return {"modelMatchesVin": modelMatchesVin, "errors": errors, "values": values, "options": vehicleOptions}
+        records = data["vehicles"]
     else:
-        limit = float("inf")
-        try:
-            csv = request.get_data().decode()
-            dataReader = VehicleDataReader(str(csv))
-        except Exception as e:
-            return "Data must be in json or csv format"
-        values = []
-        count = 0
-        matched = 0
-        errors = 0
-        notrimmatch = 0
-        totalCalls = 0
-        records = dataReader.getVehicleData()
-        for record in records.values():
-            count+=1
-            #report = {}
-            try:
-                report = kbb.getVehicleValueReportByVINAndTrim(record["VIN"], record["ModelName"] + " " + record["BodyStyle"],  "10000", "96819", record["options"])
-                if "configuredValue" in report and report["configuredValue"] > 0:
-                    matched+=1
-                if "usedLowestPricedTrim" in report and report["usedLowestPricedTrim"]:
-                    notrimmatch+=1
-                if "numCallsMade" in report:
-                    totalCalls+=report["numCallsMade"]
-            except Exception as e:
-                report = {"errors": str(e)}
-                raise e
-            if "errors" in report:
-                errors+=len(report["errors"])
-            values.append(report)
-            if count == limit:
-                break
-            if errors > count * 0.2: #If there is more than 20% error stop trying
-                break
-        
-        ret = {"count": count, "matched": matched, "errors": errors, "totalCallsMade": totalCalls, "remainingCalls": kbb.rateLimit, "usedLowestPricedTrim":notrimmatch, "values": values}
+        csv = request.get_data().decode()
+        dataReader = VehicleDataReader()
+        records = dataReader.csvInput(str(csv))
 
-        return ret
+    #----Value Vehicles-------------------------------
+    values = []
+    count = 0
+    matched = 0
+    errors = 0
+    notrimmatch = 0
+    totalCalls = 0
+
+    for record in records:
+        count+=1
+        #report = {}
+        try:
+            if "vin" in record:
+                report = kbb.getVehicleValueReportByVINAndTrim(record[dataReader.VIN], record[dataReader.TRIM],  record[dataReader.MILEAGE], "96819", record[dataReader.OPTIONS])
+            else:
+                report = kbb.getValueByName(record[dataReader.YEAR], record[dataReader.MAKE], record[dataReader.MODEL], record[dataReader.TRIM], record[dataReader.MILEAGE], "96819")
+            
+            if "configuredValue" in report and report["configuredValue"] > 0:
+                matched+=1
+            if "usedLowestPricedTrim" in report and report["usedLowestPricedTrim"]:
+                notrimmatch+=1
+            if "numCallsMade" in report:
+                totalCalls+=report["numCallsMade"]
+        except Exception as e:
+            report = {"errors": str(e)}
+            raise e
+        if "errors" in report:
+            errors+=len(report["errors"])
+        values.append(report)
+        if count == limit:
+            break
+        if errors > count * 0.2: #If there is more than 20% error stop trying
+            break
+    
+    ret = {"count": count, "matched": matched, "errors": errors, "totalCallsMade": totalCalls, "remainingCalls": kbb.rateLimit, "usedLowestPricedTrim":notrimmatch, "values": values}
+
+    return ret
 
 # def kbb_test_call(vin):
 #     params = dict()
--- vehicledatareader.py
+++ vehicledatareader.py
@@ -1,17 +1,54 @@
 import io
 import csv
 class VehicleDataReader:
-    def __init__(self, csvData) -> None:
-        csvReader = csv.DictReader(io.StringIO(csvData))
-        self.vehicleData = {}
-        for row in csvReader:
-            option = row.pop("OptionDescription", None)
-            if row["VIN"] not in self.vehicleData:
-                row["options"] = set()
-                row["options"].add(option)
-                self.vehicleData[row["VIN"]] = row
-            else:
-                self.vehicleData[row["VIN"]]["options"].add(option)
+    VIN = "vin"
+    YEAR = "year"
+    MAKE = "make"
+    MODEL = "model"
+    TRIM = "trim"
+    MILEAGE = "mileage"
+    ZIP = "zip"
+    OPTIONS = "options"
 
-    def getVehicleData(self):
-        return self.vehicleData
+    CSV_VIN_COLUMN = "VIN"
+    CSV_YEAR_COLUMN = "YEAR"
+    CSV_MAKE_COLUMN = "MakeName"
+    CSV_MODEL_COLUMN = "ModelName"
+    CSV_TRIM_COLUMN = "BodyStyle"
+    CSV_OPTION_COLUMN = "OptionDescription"
+    CSV_MILEAGE_COLUMN = ""
+
+
+    def __init__(self) -> None:
+        self.vehicleData = {}
+
+    def csvInput(self, csvData):
+        csvReader = csv.DictReader(io.StringIO(csvData))
+        for row in csvReader:
+            vin = row.pop(self.CSV_VIN_COLUMN, None)
+            year = row.pop(self.CSV_YEAR_COLUMN, None)
+            make = row.pop(self.CSV_MAKE_COLUMN, None)
+            model = row.pop(self.CSV_MODEL_COLUMN, None)
+            trim = row.pop(self.CSV_TRIM_COLUMN, None)
+            mileage = row.pop(self.CSV_MILEAGE_COLUMN, None)
+
+            if mileage and mileage.isdigit():
+                mileage = int(mileage)
+
+
+            option = row.pop(self.CSV_OPTION_COLUMN, None)
+            
+            if vin not in self.vehicleData:
+                options = set()
+                options.add(option)
+                vehicle = {self.VIN: vin, 
+                           self.YEAR: year, 
+                           self.MAKE: make, 
+                           self.MODEL: model, 
+                           self.TRIM: model + ' ' + trim, 
+                           self.MILEAGE: mileage, 
+                           self.OPTIONS: options}
+                self.vehicleData[vin] = vehicle
+            else:
+                self.vehicleData[vin]["options"].add(option)
+        return self.vehicleData.values()
